ef24e72871f3d33725661b826f1eda57bb44fd3b
unknown <waldoch.don@gmail.com>
2021-11-03 22:43:39 -0500

    11/03/21 2nd (Finished?) Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,67 @@
 // TODO: Declare any global variables we need
 
-let Side = ["Heads", "Tails"]
+var tailsCount
+var headsCount
+var flipsCount
+var txtMessage
+var imgPenny
 
-let tailsCount = 0
-let headsCount = 0
-let flipsCount = 0
+Initialize()
 
-let flip = document.querySelector('.buttonFlip')
+function Initialize(){
+    tailsCount = 0
+    headsCount = 0
+    flipsCount = 0
+    txtMessage = "Let's Get Rolling!"
+    imgPenny = "./assets/images/penny-heads.jpg"
+    Redraw()
+}
+
+let flip = document.querySelector('#flip')
 flip.addEventListener('click', function () {
-    let randomInt = getRandomInt(0,2)
     flipsCount++
-    console.log(Side[randomInt])
+    if (Math.random() < 0.5) {
+        headsCount++
+        txtMessage = "You Flipped Heads!"
+        imgPenny = "./assets/images/penny-heads.jpg"
+    } else {
+        tailsCount++
+        txtMessage = "You Flipped Tails!"
+        imgPenny = "./assets/images/penny-tails.jpg"
+    }
+    Redraw()
 })
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+let clear = document.querySelector('#clear')
+clear.addEventListener('click', function () {
+    Initialize()
+})
+
+function Redraw() {
+    let picture      = document.getElementById("penny")
+    let message      = document.getElementById("message")
+    let heads        = document.getElementById("heads")
+    let headsPercent = document.getElementById("heads-percent")
+    let tails        = document.getElementById("tails")
+    let tailsPercent = document.getElementById("tails-percent")
+
+    picture.setAttribute('src', imgPenny)
+    message.textContent = txtMessage
+    heads.textContent = headsCount
+    tails.textContent = tailsCount
+    if (flipsCount === 0) {
+        headsPercent.textContent = "0%"
+        tailsPercent.textContent = "0%"
+    } else {
+        headsPercent.textContent = Math.round(100*headsCount/flipsCount) + "%"
+        tailsPercent.textContent = Math.round(100*tailsCount/flipsCount) + "%"
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
-    
-let isHeads = Math.random() < 0.5
 
     // TODO: Add event listener and handler for flip and clear buttons
 
